e3037b0bca450f5a79df2f0b5d446d57e00574a6
Elif Sorguç <77637770+elifsorguc@users.noreply.github.com>
2024-12-03 00:33:14 +0300

    gereksiz printler removed

--- exp.c
+++ exp.c
@@ -10,6 +10,7 @@
 #define NUMT 3 // Number of threads
 
 int AVOID = 1;
+int state_changed = 0; // Track if the state has changed
 
 void pr(int tid, char astr[], int m, int r[])
 {
@@ -53,15 +54,26 @@ void *threadfunc1(void *a)
 
     setarray(request1, NUMR, 1, 0, 0, 0, 0); // Request resources
     pr(tid, "REQ", NUMR, request1);
-    reman_request(request1);
+    if (reman_request(request1) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after request by Thread 0");
+    }
     sleep(2);
 
     setarray(request2, NUMR, 0, 1, 0, 0, 0); // Request additional resources
     pr(tid, "REQ", NUMR, request2);
-    reman_request(request2);
+    if (reman_request(request2) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after additional request by Thread 0");
+    }
 
     reman_release(request1);
+    state_changed = 1;
+    reman_print("State after releasing resources by Thread 0");
     reman_release(request2);
+
     reman_disconnect();
     pthread_exit(NULL);
 }
@@ -80,15 +92,26 @@ void *threadfunc2(void *a)
 
     setarray(request1, NUMR, 0, 1, 0, 0, 0); // Request resources
     pr(tid, "REQ", NUMR, request1);
-    reman_request(request1);
+    if (reman_request(request1) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after request by Thread 1");
+    }
     sleep(3);
 
     setarray(request2, NUMR, 0, 0, 1, 0, 0); // Request additional resources
     pr(tid, "REQ", NUMR, request2);
-    reman_request(request2);
+    if (reman_request(request2) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after additional request by Thread 1");
+    }
 
     reman_release(request1);
+    state_changed = 1;
+    reman_print("State after releasing resources by Thread 1");
     reman_release(request2);
+
     reman_disconnect();
     pthread_exit(NULL);
 }
@@ -107,15 +130,26 @@ void *threadfunc3(void *a)
 
     setarray(request1, NUMR, 1, 0, 0, 0, 0); // Request resources
     pr(tid, "REQ", NUMR, request1);
-    reman_request(request1);
+    if (reman_request(request1) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after request by Thread 2");
+    }
     sleep(4);
 
     setarray(request2, NUMR, 0, 0, 0, 1, 1); // Request additional resources
     pr(tid, "REQ", NUMR, request2);
-    reman_request(request2);
+    if (reman_request(request2) == 0)
+    {
+        state_changed = 1; // Mark state as changed after allocation
+        reman_print("State after additional request by Thread 2");
+    }
 
     reman_release(request1);
+    state_changed = 1;
+    reman_print("State after releasing resources by Thread 2");
     reman_release(request2);
+
     reman_disconnect();
     pthread_exit(NULL);
 }
@@ -141,28 +175,25 @@ int main(int argc, char **argv)
         reman_init(NUMT, NUMR, 0);
 
     // Create threads with different behaviors
-    i = 0;
-    tids[i] = i;
-    pthread_create(&(threadArray[i]), NULL, (void *)threadfunc1, (void *)(void *)&tids[i]);
-
-    i = 1;
-    tids[i] = i;
-    pthread_create(&(threadArray[i]), NULL, (void *)threadfunc2, (void *)(void *)&tids[i]);
-
-    i = 2;
-    tids[i] = i;
-    pthread_create(&(threadArray[i]), NULL, (void *)threadfunc3, (void *)(void *)&tids[i]);
+    for (i = 0; i < NUMT; ++i)
+    {
+        tids[i] = i;
+        pthread_create(&(threadArray[i]), NULL, (void *)((i == 0) ? threadfunc1 : (i == 1) ? threadfunc2
+                                                                                           : threadfunc3),
+                       (void *)&tids[i]);
+    }
 
     count = 0;
     while (count < 20)
     {
         sleep(1);
-        reman_print("The current state");
-        ret = reman_detect();
+
+        ret = reman_detect(); // Detect deadlock
         if (ret > 0)
         {
             printf("deadlock detected, count=%d\n", ret);
-            reman_print("state after deadlock");
+            reman_print("State after deadlock resolution");
+            state_changed = 1; // Mark state as changed after resolving deadlock
         }
         count++;
     }
